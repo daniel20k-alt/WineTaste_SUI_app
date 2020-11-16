f21c668b02afca3a47e013cc74e95d1c6a98b84f
Testing_author <testing_beta@mail.com>
2020-11-16 23:48:36 +0200

    Initial Commit

---
+++ WineTaste_SUI_app/WineTaste_SUI_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  WineTaste_SUI_appApp.swift
+//  WineTaste_SUI_app
+//
+//  Created by DDDD on 16/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct WineTaste_SUI_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
